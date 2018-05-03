bf9bf40d05f639f68bbd13eebdd679dd084ded4c
Sagar Patel <spate334@uic.edu>
2018-05-03 03:40:26 +0000

    big update

--- projects/big/sw.js
+++ projects/big/sw.js
@@ -23,7 +23,6 @@ self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
 			.then(function(response) {
-				// Cache hit - return response
 				if (response) {
 					return response;
 				}
