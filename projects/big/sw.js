e99962f206b4d6f64990b343ca438cdc52b9348c
Sagar Patel <spate334@uic.edu>
2018-05-03 05:31:37 +0000

    big update

--- projects/big/sw.js
+++ projects/big/sw.js
@@ -2,7 +2,7 @@ var dataCacheName = 'busTracker';
 var cacheName = 'UICBusTracker';
 var urlsToCache = [
   './',
-  './imgs/uic.jpg',
+  './imgs/chicago.jpg',
   './imgs/favicon.ico',
   'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css',
   'https://fonts.googleapis.com/icon?family=Material+Icons'
